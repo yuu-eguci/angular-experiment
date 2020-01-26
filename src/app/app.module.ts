66f171520c6e9f0c37c1985646dd35aa7ec5d4c1
yuu-eguci <yuu.eguci@gmail.com>
2020-01-26 10:05:58 +0900

    #2 Execute 'ng generate component page1'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
