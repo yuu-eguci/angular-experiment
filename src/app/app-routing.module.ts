6be1368f74cd6abf43eef16dc0d7a53253b1c007
yuu-eguci <yuu.eguci@gmail.com>
2020-01-26 10:11:08 +0900

    #2 Add Page1 base
    When you access /page1, it displays page1.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { Page1Component } from './page1/page1.component';
 
 
 const routes: Routes = [
+  // TODO: I want to change the urls depending on the language such as /en/index and /ja/page1
   { path: 'index', component: IndexComponent },
+  { path: 'page1', component: Page1Component },
 ];
 
 @NgModule({
